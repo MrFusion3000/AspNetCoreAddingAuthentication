c82a876f510b9f1e2f67615ebe5a939246614c25
MrFusion3000 <nico@crepro.com>
2021-02-12 10:25:07 +0100

    v2.1

---
+++ WishList/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly SignInManager<ApplicationUser> signInManager;
+
+        public AccountController(UserManager<ApplicationUser> _userManager, SignInManager<ApplicationUser> _signInManager)
+        {
+            userManager = _userManager;
+            signInManager = _signInManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
